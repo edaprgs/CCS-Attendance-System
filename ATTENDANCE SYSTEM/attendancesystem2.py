f9fbb40005feaedf189f19a1fa9a3f23f0175344
Eda Grace Paragoso <117576162+edaprgs@users.noreply.github.com>
2023-06-29 00:24:38 +0800

    Update attendancesystem2.py

--- ATTENDANCE SYSTEM/attendancesystem2.py
+++ ATTENDANCE SYSTEM/attendancesystem2.py
@@ -32,7 +32,7 @@ create_student = '''CREATE TABLE IF NOT EXISTS student (
 conn.execute(create_student)
 
 # CREATE EVENT TABLE
-create_event = '''CREATE TABLE IF NOT EXISTS event (
+create_event = '''CREATE TABLE IF NOT EXISTS events (
 	"event_ID"	TEXT NOT NULL,
 	"eventName"	TEXT NOT NULL,
 	"start_date"	TEXT NOT NULL,
@@ -71,6 +71,18 @@ def validate_student_id(student_id):
         return False
     return True
 
+def validate_event_id(event_id):
+    conn = sqlite3.connect('attendancesystem.db')
+    cursor = conn.cursor()
+    # Check if the event ID exists in the events table
+    query = "SELECT event_ID FROM events WHERE event_ID = ?"
+    cursor.execute(query, (event_id,))
+    result = cursor.fetchone()
+    conn.close()
+    if not result:
+        return False 
+    return True
+
 def check_student_exists(student_id):
     conn = sqlite3.connect('attendancesystem.db')
     cursor = conn.cursor()
@@ -78,32 +90,46 @@ def check_student_exists(student_id):
     query = "SELECT student_ID FROM student WHERE student_ID = ?"
     cursor.execute(query, (student_id,))
     result = cursor.fetchone()
+    conn.close()
     if result:
         return True
-    conn.commit()
-    conn.close()
     return False
 
 def add_attendance(student_id, event_id, sign_type):
     conn = sqlite3.connect('attendancesystem.db')
     cursor = conn.cursor()
+    
     # Check if the attendance record already exists
     exists_query = "SELECT * FROM attends WHERE student_ID = ? AND event_ID = ?"
     cursor.execute(exists_query, (student_id, event_id))
     existing_record = cursor.fetchone()
-        
+    
     if existing_record:
-        tkMessageBox.showinfo("Attendance Recorded", "Attendance for this student at this event is already recorded.")
-    else:
-        # Add attendance record to the attends table based on the SIGN IN or SIGN OUT action
         if sign_type == 'IN':
-            query = "INSERT INTO attends (student_ID, event_ID, signin_datetime) VALUES (?, ?, datetime('now'))"
+            tkMessageBox.showinfo("Sign-in Attendance Recorded", "Sign-in Attendance for this student at this event is already recorded.")
         elif sign_type == 'OUT':
-            query = "UPDATE attends SET signout_datetime = datetime('now') WHERE student_ID = ? AND event_ID = ?"
-    cursor.execute(query, (student_id, event_id))
-    conn.commit()
-    update_student_table()
+            existing_signout = existing_record[3]  # Assuming the signout_datetime is in the fourth column
+            if existing_signout != '-':
+                tkMessageBox.showinfo("Sign-out Attendance Recorded", "Sign-out Attendance for this student at this event is already recorded.")
+            else:
+                query = "UPDATE attends SET signout_datetime = datetime('now') WHERE student_ID = ? AND event_ID = ?"
+                cursor.execute(query, (student_id, event_id))
+                conn.commit()
+                update_student_table()
+                tkMessageBox.showinfo("Signed Out", "The student has been signed out successfully.")
+    else:
+        # Add attendance record to the attends table based on the SIGN IN action
+        if sign_type == 'IN':
+            query = "INSERT INTO attends (student_ID, event_ID, signin_datetime, signout_datetime) VALUES (?, ?, datetime('now'), '-')"
+            cursor.execute(query, (student_id, event_id))
+            conn.commit()
+            update_student_table()
+            tkMessageBox.showinfo("Signed In", "The student has been signed in successfully.")
+        else:
+            tkMessageBox.showinfo("Message", "Attendance record not found.")
+    
     conn.close()
+
 
 def sign_in():
     student_id = sIDentry.get()
@@ -116,10 +142,12 @@ def sign_in():
         if not check_student_exists(student_id):
             tkMessageBox.showerror("Error", "Student ID does not exist.")
             return
-    
+        # Validate the event ID
+        if not validate_event_id(event_id):
+            tkMessageBox.showerror("Error", "Invalid event ID.")
+            return
         # Add attendance record (sign-in) to the attends table
         add_attendance(student_id, event_id, 'IN')
-        tkMessageBox.showinfo("Message","Sign-in successful.")
         return
 
 def sign_out():
@@ -134,10 +162,12 @@ def sign_out():
         if not check_student_exists(student_id):
             tkMessageBox.showerror("Error", "Student ID does not exist.")
             return
-        
+        # Validate the event ID
+        if not validate_event_id(event_id):
+            tkMessageBox.showerror("Error", "Invalid event ID.")
+            return       
         # Add attendance record (sign-out) to the attends table
         add_attendance(student_id, event_id, 'OUT')
-        tkMessageBox.showinfo("Message","Sign-out successful.")
         return
 
 #**************************************************************** UI ****************************************************************#
@@ -166,9 +196,9 @@ label2 =customtkinter.CTkLabel(frame1,text="COLLEGE OF COMPUTER STUDIES",text_co
 label2.place(x=235,y=120)
 label3 =customtkinter.CTkLabel(frame1,text="Enter Your Student Identification Number",text_color="dark slate gray",font=("Arial",15))
 label3.place(x=233,y=200)
-sIDentry = customtkinter.CTkEntry(frame1,placeholder_text="student id",font=("Roboto",35,"bold"),text_color="gray20",placeholder_text_color="gray70",border_color="lightcyan2",fg_color="white",width=450,height=60,justify="center")
+sIDentry = customtkinter.CTkEntry(frame1,placeholder_text="student id",font=("Arial",35,"bold"),text_color="gray20",placeholder_text_color="gray70",border_color="lightcyan2",fg_color="white",width=450,height=60,justify="center")
 sIDentry.place(x=140,y=240)
-eIDentry = customtkinter.CTkEntry(frame1,placeholder_text="event id",font=("Roboto",20,"bold"),text_color="gray20",placeholder_text_color="gray70",border_color="lightcyan2",fg_color="white",width=300,height=40,justify="center")
+eIDentry = customtkinter.CTkEntry(frame1,placeholder_text="event id",font=("Arial",20,"bold"),text_color="gray20",placeholder_text_color="gray70",border_color="lightcyan2",fg_color="white",width=300,height=40,justify="center")
 eIDentry.place(x=220,y=310)
 signinbtn =customtkinter.CTkButton(frame1,text="SIGN IN",text_color="white",font=("Arial",20,"bold"),fg_color="turquoise4",hover=True,hover_color= "cyan4",corner_radius=10,width=205,height=50,command=sign_in)
 signinbtn.place(x=140,y=370)
@@ -216,5 +246,4 @@ def update_student_table():
     conn.commit()
     conn.close()
 
-conn.close()
 win.mainloop()
